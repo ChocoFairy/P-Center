dbc8bcbf25ed8f5af1e2775f2e3b5849263ac647
ChocoFairy <wanlizhao@mail.dlut.edu.cn>
2021-06-08 19:58:56 +0800

    add

---
+++ P-Center/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include "Solution.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //外部调用时参数
+    string path = argv[1];
+    string output_path = argv[2];
+    int limit_s = atoi(argv[3]);
+    int rand_seed = atoi(argv[4]);
+
+    SolveStruct sol(path, output_path);
+    sol.Solve(limit_s, rand_seed);
+
+    printf("iter:%d time:%lf  uncovered_num:%d", sol.tempiter,((double)sol.end_ms - (double)sol.start_ms) / 1000.0, sol.tempnum);
+    return 0;
+}
